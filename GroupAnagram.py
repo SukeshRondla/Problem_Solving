28ef5980c393a87183c384d8c675103cf249e35d
Sukesh Reddy Rondla <65500920+sukeshredd@users.noreply.github.com>
2023-07-05 11:58:24 -0700

    Medium Problem

---
+++ GroupAnagram.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        res = {}
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+
+        return res.values()
