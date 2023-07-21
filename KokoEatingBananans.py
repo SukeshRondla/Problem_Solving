fa45557a757e7fee26c725f5b5ab483be8990e54
Sukesh Reddy Rondla <65500920+sukeshredd@users.noreply.github.com>
2023-07-20 21:24:00 -0700

    875. Koko Eating Bananas

---
+++ KokoEatingBananans.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l = 1
+        r = max(piles)
+
+        result = r
+
+        while l <= r:
+
+            k = (l + r) // 2
+
+            total_hours = 0
+
+            for pile in piles:
+                total_hours += math.ceil(pile / k)
+
+            if total_hours <= h:
+                result = min(result, k)
+                r = k - 1
+            else:
+                l = k + 1
+
+        return result
+
+
