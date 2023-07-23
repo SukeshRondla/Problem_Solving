7619ac4321eb22bacffb03ab63070c0d15fd8dda
Sukesh Reddy Rondla <65500920+sukeshredd@users.noreply.github.com>
2023-07-22 21:04:49 -0700

    167. Two Sum II - Input Array Is Sorted

---
+++ 2Sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(numbers) - 1
+
+        while l < r:
+
+            s = numbers[l] + numbers[r]
+
+            if s == target:
+                return [l + 1, r + 1]
+
+            if s > target:
+                r -= 1
+            else:
+                l += 1
+
+        return []
