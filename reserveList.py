2ccc6e9190d92d800e2eec92dc6a8bad7649f5b2
Sukesh Reddy <sukeshreddy@Sukeshs-MacBook-Pro.local>
2023-07-30 00:31:37 -0700

    Commit
    quit
    exit

---
+++ reserveList.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
