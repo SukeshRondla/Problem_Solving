df12cc76c648a11fe487fdab019459fd92b05326
Sukesh Reddy Rondla <65500920+SukeshRondla@users.noreply.github.com>
2023-10-03 20:44:47 -0500

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -1,29 +1,33 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort()
+def threeSum(nums):
+    res = []
+    nums.sort()
 
-        for i , a in enumerate(nums):
-            if i > 0 and a == nums[i-1]:
-                continue
-            
-            l = i + 1 
-            r = len(nums) - 1
+    for i, a in enumerate(nums):
+        # Skip duplicates
+        if i > 0 and a == nums[i - 1]:
+            continue
 
-            while l < r :
-                current_sum = a + nums[l] + nums[r]
+        l, r = i + 1, len(nums) - 1
 
-                if current_sum < 0:
-                    l += 1 
-                elif current_sum > 0:
-                    r -= 1 
-                else:
-                    res.append([a,nums[l],nums[r]])
-                    l += 1 
-                    while nums[l] == nums[l - 1] and l < r:
+        while l < r:
+            current_sum = a + nums[l] + nums[r]
 
-                        l += 1 
+            if current_sum < 0:
+                l += 1
+            elif current_sum > 0:
+                r -= 1
+            else:
+                # Found a valid triplet
+                res.append([a, nums[l], nums[r]])
 
-        return res
-                
-        
+                # Skip duplicates
+                while l < r and nums[l] == nums[l + 1]:
+                    l += 1
+                while l < r and nums[r] == nums[r - 1]:
+                    r -= 1
+
+                # Move pointers
+                l += 1
+                r -= 1
+
+    return res
