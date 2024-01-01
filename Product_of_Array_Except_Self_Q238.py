8fec510e23a7fbe2d0b619597496fe63f3d87296
Sukesh Reddy Rondla <65500920+SukeshRondla@users.noreply.github.com>
2024-01-01 15:58:53 -0600

    Product of Array Except Self
    nums = [1, 2, 3, 4]
    # Initialize arrays
    length = 4
    ans = [1, 1, 1, 1]
    prefix_arr = [1, 1, 1, 1]
    postfix_arr = [1, 1, 1, 1]
    
    # Calculate prefix products
    pre = 1
    for i in range(4):
        prefix_arr[i] = pre
        pre *= nums[i]
    
    # prefix_arr = [1, 1, 2, 6]  # Products of elements before each index
    
    # Calculate postfix products
    post = 1
    for i in range(3, -1, -1):
        postfix_arr[i] = post
        post *= nums[i]
    
    # postfix_arr = [24, 12, 4, 1]  # Products of elements after each index
    
    # Calculate the answer using prefix and postfix arrays
    for i in range(4):
        ans[i] = prefix_arr[i] * postfix_arr[i]
    
    # ans = [24, 12, 8, 6]  # Final result with product except self for each index
    
    return ans

---
+++ Product_of_Array_Except_Self_Q238.py
@@ -0,0 +1,40 @@
+"""
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+"""
+
+from typing import List
+
+
+def productExceptSelf(self, nums: List[int]) -> List[int]:
+    leg = len(nums)
+    ans = [1] * leg
+
+    pre_fix = [1] * leg
+
+    pre = 1
+    for i in range(leg):
+        pre_fix[i] = pre
+        pre *= nums[i]
+
+    post_fix = [1] * leg
+
+    post = 1
+    for i in range(leg - 1, -1, -1):
+        post_fix[i] = post
+        post *= nums[i]
+
+    for i in range(leg):
+        ans[i] = pre_fix[i] * post_fix[i]
+
+    return ans
